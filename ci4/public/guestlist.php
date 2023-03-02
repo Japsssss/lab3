b7c5e43e2af5621d082b1db0cc294955f8add800
jmazingw <jdgonzales2@student.apc.edu.ph>
2023-03-02 22:20:21 +0800

    ci4 Personal Website

--- ci4/public/guestlist.php
+++ ci4/public/guestlist.php
@@ -14,6 +14,9 @@
 			background-color: green;
 			border-color: green;
 		}
+		table td {
+			color: white;
+		}
 	</style>
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
 	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
