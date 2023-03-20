4223e2aede2fa4453e3d383469604b1265f53035
jmazingw <jdgonzales2@student.apc.edu.ph>
2023-03-20 15:54:28 +0800

    ci4 Personal Website update

--- ci4/app/Config/Routes.php
+++ ci4/app/Config/Routes.php
@@ -30,6 +30,7 @@ $routes->set404Override();
 // We get a performance increase by specifying the default
 // route since we don't have to scan directories.
 $routes->get('/', 'Home::index');
+$routes->get('/home', 'Home::index');
 use App\public\home\guestlist as guestslist;
 use App\public\home\contact as contacts;
 use App\Controllers\Guest;
